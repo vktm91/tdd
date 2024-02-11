6f46c5546e70bfb45dda8836c905da41d7a9ab17
dadmi <vktm91@nate.com>
2024-02-12 00:05:18 +0900

    feat: [실기로 배우는 간단한 테스트 작성 방법 소개] 실습 코드

---
+++ spring-test-error-note/src/test/java/com/example/sample/CalculationRequestReaderTest.java
@@ -0,0 +1,25 @@
+package com.example.sample;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CalculationRequestReaderTest {
+
+    @Test
+    public void System_in으로_데이터를_읽어들일_수_있다() {
+        // given
+        CalculationRequestReader calculationRequestReader = new CalculationRequestReader();
+
+        // when
+        System.setIn(new ByteArrayInputStream("2 + 3".getBytes()));
+        CalculationRequest result = calculationRequestReader.read();
+
+        // then
+        assertEquals(2, result.getNum1());
+        assertEquals("+", result.getOperator());
+        assertEquals(3, result.getNum2());
+    }
+}
