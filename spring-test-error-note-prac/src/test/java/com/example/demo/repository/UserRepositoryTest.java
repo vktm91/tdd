a2525c398bc87d78a01db29ba9fff00f4c4da799
dadm <vktm91@nate.com>
2024-02-22 22:46:17 +0900

    feat: repository test 작성과 @Sql 사용

---
+++ spring-test-error-note-prac/src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -0,0 +1,67 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.UserStatus;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+// 개별로 테스트 할 때는 성공하는데, 전체 테스트를 돌릴 때는 실패한다.
+// 테스트 메서드가 병렬로 처리되는데 동시성 제어가 안 되기 떄문이다.
+//  @Sql을 사용하면 각 테스트 메서드 또는 클래스 전후에 데이터베이스를 특정 상태로 초기화할 수 있다. 이는 테스트 간의 데이터 충돌을 방지하고 각 테스트가 독립적인 환경에서 실행될 수 있도록 보장한다.
+@Sql("/sql/user-repository-test-data.sql")
+@DataJpaTest(showSql = false)
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void findByIdAndStatus_로_유저_데이터를_찾아올_수_있다() {
+        //given
+
+        // when
+        Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.ACTIVE);
+
+        // then
+        assertThat(result.isPresent()).isTrue();
+    }
+
+    @Test
+    void findByIdAndStatus_는_데이터가_없으면_Optional_empty_를_내려준다() {
+        //given
+
+        // when
+        Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.PENDING);
+
+        // then
+        assertThat(result.isEmpty()).isTrue();
+    }
+
+    @Test
+    void findByEmailAndStatus_로_유저_데이터를_찾아올_수_있다() {
+        //given
+
+        // when
+        Optional<UserEntity> result = userRepository.findByEmailAndStatus("kok202@naver.com", UserStatus.ACTIVE);
+
+        // then
+        assertThat(result.isPresent()).isTrue();
+    }
+
+    @Test
+    void findByEmailAndStatus_는_데이터가_없으면_Optional_empty_를_내려준다() {
+        //given
+
+        // when
+        Optional<UserEntity> result = userRepository.findByEmailAndStatus("kok202@naver.com", UserStatus.PENDING);
+
+        // then
+        assertThat(result.isEmpty()).isTrue();
+    }
+
+}
