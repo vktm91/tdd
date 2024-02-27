b4f58aa3723b5c1e1e8d624599ad1e21dd0f6c64
dadm <vktm91@nate.com>
2024-02-27 21:25:25 +0900

    refactor: 불필요한 코드 제거 및 리네이밍

--- spring-test-error-note-prac/src/main/java/com/example/demo/controller/UserCreateController.java
+++ spring-test-error-note-prac/src/main/java/com/example/demo/controller/UserCreateController.java
@@ -23,8 +23,8 @@ public class UserCreateController {
     private final UserService userService;
 
     @PostMapping
-    public ResponseEntity<UserResponse> createUser(@RequestBody UserCreateDto userCreateDto) {
-        UserEntity userEntity = userService.createUser(userCreateDto);
+    public ResponseEntity<UserResponse> create(@RequestBody UserCreateDto userCreateDto) {
+        UserEntity userEntity = userService.create(userCreateDto);
         return ResponseEntity
             .status(HttpStatus.CREATED)
             .body(userController.toResponse(userEntity));
