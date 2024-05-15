eaf88cfc35d6348bdd7629ae572d459029c30254
dadm <vktm91@nate.com>
2024-05-15 23:12:46 +0900

    fix: userService, UserController에 도메인과 영속성 객체 분리 적용

--- spring-test-error-note-prac-refactoring/src/main/java/com/example/demo/user/service/UserService.java
+++ spring-test-error-note-prac-refactoring/src/main/java/com/example/demo/user/service/UserService.java
@@ -1,20 +1,14 @@
 package com.example.demo.user.service;
 
-
-import com.example.demo.common.domain.exception.CertificationCodeNotMatchedException;
 import com.example.demo.common.domain.exception.ResourceNotFoundException;
+import com.example.demo.user.domain.User;
 import com.example.demo.user.domain.UserCreate;
 import com.example.demo.user.domain.UserStatus;
 import com.example.demo.user.domain.UserUpdate;
-import com.example.demo.user.infrastructure.UserEntity;
 import com.example.demo.user.service.port.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.Clock;
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -23,51 +17,44 @@ public class UserService {
     private final UserRepository userRepository;
     private final CertificationService certificationService;
 
-    public UserEntity getByEmail(String email) {
+    public User getByEmail(String email) {
         return userRepository.findByEmailAndStatus(email, UserStatus.ACTIVE)
             .orElseThrow(() -> new ResourceNotFoundException("Users", email));
     }
 
-    public UserEntity getById(long id) {
+    public User getById(long id) {
         return userRepository.findByIdAndStatus(id, UserStatus.ACTIVE)
             .orElseThrow(() -> new ResourceNotFoundException("Users", id));
     }
 
     @Transactional
-    public UserEntity create(UserCreate userCreate) {
-        UserEntity userEntity = new UserEntity();
-        userEntity.setEmail(userCreate.getEmail());
-        userEntity.setNickname(userCreate.getNickname());
-        userEntity.setAddress(userCreate.getAddress());
-        userEntity.setStatus(UserStatus.PENDING);
-        userEntity.setCertificationCode(UUID.randomUUID().toString());
-        userEntity = userRepository.save(userEntity);
-        certificationService.send(userCreate.getEmail(), userEntity.getId(), userEntity.getCertificationCode());
-        return userEntity;
+    public User create(UserCreate userCreate) {
+        User user = User.from(userCreate);
+        user = userRepository.save(user);
+        certificationService.send(userCreate.getEmail(), user.getId(), user.getCertificationCode());
+        return user;
     }
 
     @Transactional
-    public UserEntity update(long id, UserUpdate userUpdateDto) {
-        UserEntity userEntity = getById(id);
-        userEntity.setNickname(userUpdateDto.getNickname());
-        userEntity.setAddress(userUpdateDto.getAddress());
-        userEntity = userRepository.save(userEntity);
-        return userEntity;
+    public User update(long id, UserUpdate userUpdate) {
+        User user = getById(id);
+        user.update(userUpdate);
+        user = userRepository.save(user);
+        return user;
     }
 
     @Transactional
     public void login(long id) {
-        UserEntity userEntity = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Users", id));
-        userEntity.setLastLoginAt(Clock.systemUTC().millis());
+        User user = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Users", id));
+        user = user.login();
+        userRepository.save(user);   // 기존에는 set만 했지만 save까지 해주는 이유: 변경 대상이 도메인 객체지, 영속성 객체가 아니기 때문에 jpa와의 의존성이 끊어져서 변경감지로 인한 자동 저장이 안되므로, save까지 해주는것.
     }
 
     @Transactional
     public void verifyEmail(long id, String certificationCode) {
-        UserEntity userEntity = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Users", id));
-        if (!certificationCode.equals(userEntity.getCertificationCode())) {
-            throw new CertificationCodeNotMatchedException();
-        }
-        userEntity.setStatus(UserStatus.ACTIVE);
+        User user = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Users", id));
+        user.certification(certificationCode);
+        userRepository.save(user);
     }
 
 }
--- spring-test-error-note-prac-refactoring/src/test/java/com/example/demo/user/service/CertificationServiceTest.java
+++ spring-test-error-note-prac-refactoring/src/test/java/com/example/demo/user/service/CertificationServiceTest.java
@@ -1,7 +1,6 @@
 package com.example.demo.user.service;
 
 import com.example.demo.mock.FakeMailSender;
-import com.example.demo.user.service.port.MailSender;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
