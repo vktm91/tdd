0fd733d0a88926d105e0720fb14325da445c1a0c
dadm <vktm91@nate.com>
2024-05-23 00:23:46 +0900

    feat: user test 작성

--- spring-test-error-note-prac-refactoring/src/main/java/com/example/demo/user/service/UserService.java
+++ spring-test-error-note-prac-refactoring/src/main/java/com/example/demo/user/service/UserService.java
@@ -1,6 +1,8 @@
 package com.example.demo.user.service;
 
 import com.example.demo.common.domain.exception.ResourceNotFoundException;
+import com.example.demo.common.service.port.ClockHolder;
+import com.example.demo.common.service.port.UuidHolder;
 import com.example.demo.user.domain.User;
 import com.example.demo.user.domain.UserCreate;
 import com.example.demo.user.domain.UserStatus;
@@ -16,6 +18,8 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final CertificationService certificationService;
+    private final UuidHolder uuidHolder;
+    private final ClockHolder clockHolder;
 
     public User getByEmail(String email) {
         return userRepository.findByEmailAndStatus(email, UserStatus.ACTIVE)
@@ -29,7 +33,7 @@ public class UserService {
 
     @Transactional
     public User create(UserCreate userCreate) {
-        User user = User.from(userCreate);
+        User user = User.from(userCreate, uuidHolder);
         user = userRepository.save(user);
         certificationService.send(userCreate.getEmail(), user.getId(), user.getCertificationCode());
         return user;
@@ -46,7 +50,7 @@ public class UserService {
     @Transactional
     public void login(long id) {
         User user = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Users", id));
-        user = user.login();
+        user = user.login(clockHolder);
         userRepository.save(user);   // 기존에는 set만 했지만 save까지 해주는 이유: 변경 대상이 도메인 객체지, 영속성 객체가 아니기 때문에 jpa와의 의존성이 끊어져서 변경감지로 인한 자동 저장이 안되므로, save까지 해주는것.
     }
 
--- spring-test-error-note-prac-refactoring/src/test/java/com/example/demo/user/domain/UserTest.java
+++ spring-test-error-note-prac-refactoring/src/test/java/com/example/demo/user/domain/UserTest.java
@@ -1,56 +1,123 @@
 package com.example.demo.user.domain;
 
+import com.example.demo.common.domain.exception.CertificationCodeNotMatchedException;
+import com.example.demo.mock.TestClockHolder;
+import com.example.demo.mock.TestUuidHolder;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class UserTest {
 
     @Test
     public void UserCreate_객체로_생성_할_수_있다() {
         // given
+        UserCreate userCreate = UserCreate.builder()
+                .email("kok202@naver.com")
+                .nickname("kok202")
+                .address("Gyeongi")
+                .build();
 
         // when
+        User user = User.from(userCreate, new TestUuidHolder("aaaa-aaaa-aaaa-aaaa"));
 
         // then
-
+        assertThat(user.getId()).isNull();
+        assertThat(user.getEmail()).isEqualTo("kok202@naver.com");
+        assertThat(user.getNickname()).isEqualTo("kok202");
+        assertThat(user.getAddress()).isEqualTo("Gyeongi");
+        assertThat(user.getStatus()).isEqualTo(UserStatus.PENDING);
+        assertThat(user.getCertificationCode()).isEqualTo("aaaa-aaaa-aaaa-aaaa");
     }
 
     @Test
     public void UserUpdate_객체로_데이터를_업데이트_할_수_있다() {
         // given
+        User user = User.builder()
+                .id(1L)
+                .email("kok202@naver.com")
+                .nickname("kok202")
+                .address("Seoul")
+                .status(UserStatus.ACTIVE)
+                .lastLoginAt(100L)
+                .certificationCode("aaaa-aaaa-aaaa-aaaa")
+                .build();
+
+        UserUpdate userUpdate = UserUpdate.builder()
+                .nickname("kok202-k")
+                .address("YangJae")
+                .build();
 
         // when
+        user = user.update(userUpdate);
 
         // then
-
+        assertThat(user.getId()).isEqualTo(1L);
+        assertThat(user.getEmail()).isEqualTo("kok202@naver.com");
+        assertThat(user.getNickname()).isEqualTo("kok202-k");
+        assertThat(user.getAddress()).isEqualTo("YangJae");
+        assertThat(user.getStatus()).isEqualTo(UserStatus.ACTIVE);
+        assertThat(user.getLastLoginAt()).isEqualTo(100L);
+        assertThat(user.getCertificationCode()).isEqualTo("aaaa-aaaa-aaaa-aaaa");
     }
 
     @Test
     public void 로그인을_할_수_있고_로그인시_마지막_로그인_시간이_변경된다() {
         // given
+        User user = User.builder()
+                .id(1L)
+                .email("kok202@naver.com")
+                .nickname("kok202")
+                .address("Seoul")
+                .status(UserStatus.ACTIVE)
+                .lastLoginAt(100L)
+                .certificationCode("aaaa-aaaa-aaaa-aaaa")
+                .build();
 
         // when
+        user = user.login(new TestClockHolder(1678530673958L));
 
         // then
-
+        assertThat(user.getLastLoginAt()).isEqualTo(1678530673958L);
     }
 
     @Test
     public void 유효한_인증_코드로_계정을_활성화_할_수_있다() {
         // given
+        User user = User.builder()
+                .id(1L)
+                .email("kok202@naver.com")
+                .nickname("kok202")
+                .address("Seoul")
+                .status(UserStatus.PENDING)
+                .lastLoginAt(100L)
+                .certificationCode("aaaa-aaaa-aaaa-aaaa")
+                .build();
 
         // when
+        user = user.certification("aaaa-aaaa-aaaa-aaaa");
 
         // then
-
+        assertThat(user.getStatus()).isEqualTo(UserStatus.ACTIVE);
     }
 
     @Test
     public void 잘못된_인증_코드로_계정을_활성화_하려하면_에러를_던진다() {
         // given
+        User user = User.builder()
+                .id(1L)
+                .email("kok202@naver.com")
+                .nickname("kok202")
+                .address("Seoul")
+                .status(UserStatus.PENDING)
+                .lastLoginAt(100L)
+                .certificationCode("aaaa-aaaa-aaaa-aaaa")
+                .build();
 
         // when
-
         // then
-
+        assertThatThrownBy(() -> user.certification("bbbb-aaaa-aaaa-bbbb"))
+                .isInstanceOf(CertificationCodeNotMatchedException.class);
     }
 }
