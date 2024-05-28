ef201799c3e02ff77001c102c5d461f85b94e35a
dadm <vktm91@nate.com>
2024-05-28 21:52:07 +0900

    feat: clockHolder 사용안한곳 적용

--- spring-test-error-note-prac-refactoring/src/main/java/com/example/demo/post/domain/Post.java
+++ spring-test-error-note-prac-refactoring/src/main/java/com/example/demo/post/domain/Post.java
@@ -1,10 +1,9 @@
 package com.example.demo.post.domain;
 
+import com.example.demo.common.service.port.ClockHolder;
 import com.example.demo.user.domain.User;
 import lombok.Builder;
 import lombok.Getter;
-
-import java.time.Clock;
 
 @Getter
 public class Post {
@@ -23,21 +22,21 @@ public class Post {
         this.writer = writer;
     }
 
-    public static Post from(User writer, PostCreate postCreate) {
+    public static Post from(User writer, PostCreate postCreate, ClockHolder clockHolder) {
         return Post.builder()
                 .content(postCreate.getContent())
-                .createdAt(Clock.systemUTC().millis())
+                .createdAt(clockHolder.millis())
                 .writer(writer)
                 .build();
 
     }
 
-    public Post update(PostUpdate postUpdate) {
+    public Post update(PostUpdate postUpdate, ClockHolder clockHolder) {
         return Post.builder()
                 .id(id)
                 .content(postUpdate.getContent())
                 .createdAt(createdAt)
-                .modifiedAt(Clock.systemUTC().millis())
+                .modifiedAt(clockHolder.millis())
                 .writer(writer)
                 .build();
     }
--- spring-test-error-note-prac-refactoring/src/test/java/com/example/demo/post/domain/PostTest.java
+++ spring-test-error-note-prac-refactoring/src/test/java/com/example/demo/post/domain/PostTest.java
@@ -1,5 +1,6 @@
 package com.example.demo.post.domain;
 
+import com.example.demo.mock.TestClockHolder;
 import com.example.demo.user.domain.User;
 import com.example.demo.user.domain.UserStatus;
 import org.junit.jupiter.api.Test;
@@ -25,10 +26,11 @@ public class PostTest {
                 .build();
 
         // when
-        Post post = Post.from(writer, postCreate);
+        Post post = Post.from(writer, postCreate, new TestClockHolder(1679530673958L));
 
         // then
         assertThat(post.getContent()).isEqualTo("오늘은 요다랑 순대곱창을 먹었어요");
+        assertThat(post.getCreatedAt()).isEqualTo((1679530673958L));
         assertThat(post.getWriter().getEmail()).isEqualTo("kok202@naver.com");
         assertThat(post.getWriter().getNickname()).isEqualTo("kok202");
         assertThat(post.getWriter().getAddress()).isEqualTo("Seoul");
@@ -36,4 +38,34 @@ public class PostTest {
         assertThat(post.getWriter().getCertificationCode()).isEqualTo("aaaa-aaaa-aaaa-aaaa");
     }
 
+    @Test
+    public void PostUpdate으로_게시물을_수정할_수_있다() {
+        // given
+        PostUpdate postUpdate = PostUpdate.builder()
+                .content("오늘은 요다랑 두찜 묵은지 찜닭을 먹었어요")
+                .build();
+
+        User writer = User.builder()
+                .email("kok202@naver.com")
+                .nickname("kok202")
+                .address("Seoul")
+                .status(UserStatus.ACTIVE)
+                .certificationCode("aaaa-aaaa-aaaa-aaaa")
+                .build();
+
+        Post post = Post.builder()
+                .id(1L)
+                .content("helloworld")
+                .createdAt(1678530673958L)
+                .modifiedAt(0L)
+                .writer(writer)
+                .build();
+
+        // when
+        post = post.update(postUpdate, new TestClockHolder(1679530673958L));
+
+        // then
+        assertThat(post.getContent()).isEqualTo("오늘은 요다랑 두찜 묵은지 찜닭을 먹었어요");
+        assertThat(post.getModifiedAt()).isEqualTo((1679530673958L));
+    }
 }
